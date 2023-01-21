1d2ccec178118d42516e0adc3a6ad392925375a9
zsszurovecz <zsolt.szurovecz@getbridge.com>
2023-01-21 15:29:01 +0100

    Update LoginScreenTests.java

--- src/test/java/hu/progmatic/swaglabs/LoginScreenTests.java
+++ src/test/java/hu/progmatic/swaglabs/LoginScreenTests.java
@@ -28,19 +28,19 @@ public class LoginScreenTests extends DriverBaseTest {
         Thread.sleep(2000);
 
         // megkeressük a hibaüzenetet a felületen (bármely olyan webelement, ahol a szöveg a html tag-ek között az, hogy Epic sadface: Password is required )
-        WebElement errorMessage = driver.findElement(By.xpath("//*[text()='Epic sadface: Password is required']"));
+        // WebElement errorMessage = driver.findElement(By.xpath("//*[text()='Epic sadface: Password is required']"));
         // VAGY megkeressük a hibaüzenetet oly módon, hogy azon h3 html tag, amelyiknek a data-test attribútumának az értéke az, hogy "error"
-        //WebElement errorMessage = driver.findElement(By.cssSelector("h3[data-test='error']"));
+        WebElement errorMessage = driver.findElement(By.cssSelector("h3[data-test='error']"));
 
         // annak ellenőrzése, hogy az elvárt hibaüzenetet tartalmazza-e az errorMessage html element tag-ek közötti értéke
         Assert.assertEquals("Epic sadface: Password is required", errorMessage.getText());
         // VAGY annak ellenőrzése, hogy az előzetesen megkeresett hibát tartalmazó html element valóban megjelent-e a felületen vagy tovább navigált az oldal
-        Assert.assertTrue(errorMessage.isDisplayed());
+        // Assert.assertTrue(errorMessage.isDisplayed());
 
         // PÉLDA assertek
-        //Assert.assertEquals("alma","korte"); //failed
-        //Assert.assertEquals("alma","alma"); //passed
-        //Assert.assertTrue(5==7); // failed
-        //Assert.assertTrue(7==7); // passed
+        // Assert.assertEquals("alma","korte"); //failed
+        // Assert.assertEquals("alma","alma"); //passed
+        // Assert.assertTrue(5==7); // failed
+        // Assert.assertTrue(7==7); // passed
     }
 }
