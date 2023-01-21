69f7547eff4ce4a7061e7951f312fdb6193fc3e6
zsszurovecz <zsolt.szurovecz@getbridge.com>
2023-01-21 15:18:26 +0100

    Update LoginScreenTests.java

--- src/test/java/hu/progmatic/swaglabs/LoginScreenTests.java
+++ src/test/java/hu/progmatic/swaglabs/LoginScreenTests.java
@@ -12,20 +12,35 @@ public class LoginScreenTests extends DriverBaseTest {
     @Test
     public void validUsernameEmptyPwdTest() throws InterruptedException {
         driver.get("https://www.saucedemo.com");
+
+        // megkeressük a username mezőt és kitöltjük helyes username-el
         WebElement usernameInput = driver.findElement(By.id("user-name"));
         usernameInput.sendKeys("standard_user");
+
+        // megkeressük a password mezőt, pusztán azért, hogy ezáltal ellenőrzni tudjuk annak megjelenését
         WebElement pwdInput = driver.findElement(By.id("password"));
 
+        // megkeressük a login gombot és rákattintunk
         WebElement loginButton = driver.findElement(By.id("login-button"));
         loginButton.click();
-        Thread.sleep(3000);
 
+        // kattintás után várunk 2mp-et, hogy a szem le tudja követni mi történik (a teszeset végeztével törölendő, hogy gyorsabban fusson)
+        Thread.sleep(2000);
+
+        // megkeressük a hibaüzenetet a felületen (bármely olyan webelement, ahol a szöveg a html tag-ek között az, hogy Epic sadface: Password is required )
         WebElement errorMessage = driver.findElement(By.xpath("//*[text()='Epic sadface: Password is required']"));
+        // VAGY megkeressük a hibaüzenetet oly módon, hogy azon h3 html tag, amelyiknek a data-test attribútumának az értéke az, hogy "error"
+        // WebElement errorMessage = driver.findElement(By.cssSelector("h3[data-test='error']"));
+
+        // annak ellenőrzése, hogy az elvárt hibaüzenetet tartalmazza-e az errorMessage html element tag-ek közötti értéke
         Assert.assertEquals("Epic sadface: Password is required", errorMessage.getText());
-        // VAGY
+        // VAGY annak ellenőrzése, hogy az előzetesen megkeresett hibát tartalmazó html element valóban megjelent-e a felületen vagy tovább navigált az oldal
         Assert.assertTrue(errorMessage.isDisplayed());
 
+        // PÉLDA assertek
         //Assert.assertEquals("alma","korte"); //failed
         //Assert.assertEquals("alma","alma"); //passed
+        //Assert.assertTrue(5==7); // failed
+        //Assert.assertTrue(7==7); // passed
     }
 }
