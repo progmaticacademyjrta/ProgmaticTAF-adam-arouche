d463918c6b3d7b404957b10e52271d0ef7d7acb0
zsszurovecz <zsolt.szurovecz@getbridge.com>
2023-01-25 18:10:50 +0100

    add webelement list example

--- src/test/java/hu/progmatic/swaglabs/ProductScreenTests.java
+++ src/test/java/hu/progmatic/swaglabs/ProductScreenTests.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductScreenTests extends DriverBaseTest {
 
     @BeforeEach
@@ -18,23 +21,32 @@ public class ProductScreenTests extends DriverBaseTest {
 
     @Test
     public void defaultSortOptionTest() throws InterruptedException {
-        WebElement selectedFilterOption = driver
-        .findElement(By.xpath("//div[@class='right_component']//span[@class='active_option']"));
-        Assert.assertEquals ("NAME (A TO Z)", selectedFilterOption.getText()); 
+        WebElement selectedFilterOption = driver.findElement(By.xpath("//div[@class='right_component']//span[@class='active_option']"));
+        Assert.assertEquals ("NAME (A TO Z)", selectedFilterOption.getText());
     }
 
     @Test
-    public void firstItemInAToZTest() throws InterruptedException {
+    public void firstItemInAToZDraftTest() throws InterruptedException {
         WebElement firstElement = driver.findElement(By.id("item_4_title_link"));
         Assert.assertEquals ("Sauce Labs Backpack", firstElement.getText());
         // We could get all the items in a list, and check the texts
         // List<WebElement> items = driver.findElements(By.className("inventory_item_name"));
-    } 
+    }
+
+    @Test
+    public void firstItemInAToZTest() throws InterruptedException {
+        List<WebElement> items = driver.findElements(By.className("inventory_item_name"));
+        /*for (WebElement item : items) {
+            System.out.println(item.getText());
+        }*/
+        String firstItemTitle = items.get(0).getText();
+        Assert.assertEquals ("Sauce Labs Backpack", firstItemTitle);
+    }
 
     private void login() throws InterruptedException {
         WebElement usernameInput  = driver.findElement(By.id("user-name"));
-        WebElement pwdInput = driver.findElement(By.id("password"));
         usernameInput.sendKeys("standard_user");
+        WebElement pwdInput = driver.findElement(By.id("password"));
         pwdInput.sendKeys("secret_sauce");
         WebElement loginButton = driver.findElement(By.id("login-button"));
         loginButton.click();
